f63e362b9cf1e049e12cb6cb9f2840b4eb679e63
Finja Koehn <volmtje@gmail.com>
2024-01-28 15:33:29 +0100

    refactored: comments and indentions for readability

--- Assets/Scripts/PolygonMap/MapMaker.cs
+++ Assets/Scripts/PolygonMap/MapMaker.cs
@@ -7,33 +7,38 @@ public class MapMaker : MonoBehaviour
     [SerializeField] Material material;
     [SerializeField] int polygonAmmount;
 
+    // Draw Polygon Map to the GameScreen
     private void Start()
     {
         Vector2 screenSize;
         screenSize.x = Screen.width;
         screenSize.y = Screen.height;
 
+        //get voronoi graph
         FortunesVoronoiGraph fortune = new FortunesVoronoiGraph(Vector2.zero, screenSize, polygonAmmount);
         List<Vector2> points = fortune.GetPoints();
         List<Edge> edges = fortune.GetEdges();
 
-        PolygonCalculator pc = new PolygonCalculator(screenSize, points, edges); // prep the polygons
+        //get polygons from the voronoi graph
+        PolygonCalculator pc = new PolygonCalculator(screenSize, points, edges);
 
+        // Draw polygons to the screen
         foreach (var polygon in pc.GetPolygons())
             MakePolygonGameObject(polygon.Key, polygon.Value);
     }
 
 
-    //this function takes the orgin and cornerpoints of a polygon
-    //and creates a gameobject to the scene root with a mesh of that polygon
+    // This function makes the given polygon center and corner into a mesh and adds it to the scene
     void MakePolygonGameObject(Vector2 position, List<Vector2> corners)
     {
+        // Create the mesh
         PolygonMeshGenerator meshGenerator = new PolygonMeshGenerator(position, corners);
         Mesh mesh = meshGenerator.GetPolygonMesh();
 
         Material meshMaterial = new Material(material);
         meshMaterial.color = Random.ColorHSV();
 
+        //Create the Gameobject
         GameObject polygonObject = new GameObject("polygonField", typeof(MeshFilter), typeof(MeshRenderer));
         polygonObject.GetComponent<MeshFilter>().mesh = mesh;
         polygonObject.GetComponent<MeshRenderer>().material = meshMaterial;
