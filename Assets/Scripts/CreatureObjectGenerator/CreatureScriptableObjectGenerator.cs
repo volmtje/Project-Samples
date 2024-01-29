88b58b59480622942a5727ef14e5bd0905abec72
Finja Koehn <volmtje@gmail.com>
2024-01-29 12:08:12 +0100

    BugFix: implemented missing validation cases - for null sprite and incorrect csv fields

--- Assets/Scripts/CreatureObjectGenerator/CreatureScriptableObjectGenerator.cs
+++ Assets/Scripts/CreatureObjectGenerator/CreatureScriptableObjectGenerator.cs
@@ -30,6 +30,7 @@ public class CreatureScriptableObjectGenerator : MonoBehaviour
             //setup creature object
             Creature c = ScriptableObject.CreateInstance<Creature>();
             c.id = int.Parse(data[0]);
+            c.artwork = GetCreatureSprite(data[0]);
             c.name = data[1];
             c.description = data[2];
             c.location = (Creature.Locations)Enum.Parse(typeof(Creature.Locations), data[3]);
@@ -43,16 +44,13 @@ public class CreatureScriptableObjectGenerator : MonoBehaviour
             c.charmMax = int.Parse(data[11]);
             c.rarity = int.Parse(data[12]);
 
-            string id = c.id.ToString();
+            //add creature to asset files
+            string id = data[0];
             if (c.id < 10)
                 id = "00" + id;
             else if (c.id < 100)
                 id = "0" + id;
 
-            //assign sprite artwork from files
-            c.artwork = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Creatures/" + id + ".png");
-
-            //add creature to asset files
             if (!AssetDatabase.IsValidFolder("Assets/Creatures/"))
                 AssetDatabase.CreateFolder("Assets", "Creatures");
             AssetDatabase.CreateAsset(c, "Assets/Creatures/" + id + ".asset");
@@ -67,11 +65,25 @@ public class CreatureScriptableObjectGenerator : MonoBehaviour
         AssetDatabase.SaveAssets();
     }
 
+    // Checks if all the data for a creature is valid
     private bool IsCreatureDataValid(string[] data)
     {
+
+        if (data.Length < 13)   //skip for incomplete creature entries
+        {
+            Debug.Log($"CSV Field seperation is incorrect");
+            return false;
+        }
+
         foreach (string s in data)  //skip incomplete creature entries
             if (String.IsNullOrEmpty(s))
                 return false;
+
+        if (GetCreatureSprite(data[0]) == null) //skip for missing sprite
+        {
+            Debug.LogError($"Invalid sprite for creature number {data[0]}");
+            return false;
+        }
 
         Creature.Locations creatureLocation;
         if (!Enum.TryParse(data[3], out creatureLocation)) //skip for invalid location name
@@ -81,5 +93,17 @@ public class CreatureScriptableObjectGenerator : MonoBehaviour
         }
 
         return true;
+    }
+
+    // Gets the sprite artwork from files
+    private Sprite GetCreatureSprite(string id)
+    {
+        int intID = int.Parse(id);
+        if (intID < 10)
+            id = "00" + id;
+        else if (intID < 100)
+            id = "0" + id;
+        Sprite artwork = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Creatures/" + id + ".png");
+        return artwork;
     }
 }
